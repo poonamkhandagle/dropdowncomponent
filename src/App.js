db12b06b2d62af9cf5213e90c7494db7c0b5318a
poonam-khandagle <poonam@kudosfinance.in>
2021-03-09 11:55:18 +0530

    created an array of options to display in the dropdown and make a list out of it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Dropdown from "./components/Dropdown";
  const App = () => {
+   const options = [
+     {
+       label:"The Color Red",
+       value:"Red"
+     },
+     {
+       label:"The Color Green",
+       value:"Green"
+     },
+     {
+       label:"A Shade Of Blue",
+       value:"Blue"
+     }
+   ]
    return(
-     <div>Dropdown component</div>
+     <div><Dropdown options={options} /></div>
    )
  }
  export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Dropdown = ( {options} ) => {
+  const renderedOptions = options.map((option) => {
+    return(
+      <div key={option.value}>
+      {option.label}
+      </div>
+    )
+  })
+  return(
+    <div>{renderedOptions}</div>
+  )
+}
+export default Dropdown;
