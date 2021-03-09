74a7101d20864a0351daac8a11eb2d934b10a778
poonam-khandagle <poonam@kudosfinance.in>
2021-03-09 16:31:49 +0530

    when we click outside of dropdown component i.e.body dropdown should close using useRef hook

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,17 @@
-import React, {useState} from "react";
+import React, { useState, useEffect, useRef } from "react";
 const Dropdown = ( { options, selected, onSelectedChange } ) => {
-  const [open,setOpen] = useState(false);
+const [open,setOpen] = useState(false);
+const ref = useRef();
+
+  useEffect(() => {
+   document.body.addEventListener('click', (event) => {
+     if (ref.current && ref.current.contains(event.target)) {
+       return;
+     }
+
+     setOpen(false);
+   });
+ }, []);
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value){
       return null;
@@ -15,19 +26,19 @@ const Dropdown = ( { options, selected, onSelectedChange } ) => {
     )
   })
   return(
-    <div className="ui form">
-    <div className="field">
-    <label className="label">Select a Color</label>
-    <div
-    onClick= {() => setOpen(!open)}
-    className={`ui selection dropdown ${open ? 'visible active' : " "}`} >
-    <i className="dropdown icon"></i>
-    <div className="text">{selected.label}</div>
-    <div className = {`menu ${open ? 'visible transition' : " "}`} >
-    {renderedOptions}
-    </div>
-    </div>
-    </div>
+    <div ref={ref} className="ui form">
+      <div className="field">
+       <label className="label">Select a Color</label>
+        <div
+         onClick= {() => setOpen(!open)}
+         className={`ui selection dropdown ${open ? 'visible active' : " "}`} >
+        <i className="dropdown icon"></i>
+       <div className="text">{selected.label}</div>
+       <div className = {`menu ${open ? 'visible transition' : " "}`} >
+         {renderedOptions}
+        </div>
+       </div>
+      </div>
     </div>
   )
 }
